8cd0254d705ad8af372315517b1197d9c631a0b8
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-11-09 20:48:29 +0100

    feat: Create inputs with linked props in login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,43 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css"
+import { CustomInput } from "../../common/CustomInput/CustomInput";
 
-export const Login = ()=>{
-    return(
-        <div className="login-body"> Soy El Login </div>
+export const Login = () => {
+
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: "",
+    });
+
+    const functionHandler = (e) => {
+        setCredentials((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    // useEffect(() => {
+    //     console.log(credentials);
+    // }, [credentials]);
+
+    return (
+        <div className="login-body">
+
+            <CustomInput
+                design={"inputDesign"}
+                type={"email"}
+                name={"email"}
+                placeholder={"user@gmail.com"}
+                functionProp={functionHandler}
+            />
+            <CustomInput
+                design={"inputDesign"}
+                type={"password"}
+                name={"password"}
+                placeholder={"Aa1234@"}
+                functionProp={functionHandler}
+            />
+
+        </div>
     )
 }
