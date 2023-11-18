f74de33bf6431b64abee00b0d10b998cae914603
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-11-18 19:28:35 +0100

    refactor: introduce a ternary to toggle in card appointment

--- src/common/CardsAppointment/CardsAppointment.jsx
+++ src/common/CardsAppointment/CardsAppointment.jsx
@@ -3,7 +3,7 @@ import "./CardsAppointment.css"
 import { LinkButton } from "../LinkButton/LinkButton";
 import { LinkDelete } from "../LinkDelete/LinkDelete";
 
-export const CardsAppointments = ({ nameProduct, imageProduct, categoryProduct, emailWorker, nameWorker, appointmentId, date, shift, price, emit, client_name, client_email, status,deleted }) => {
+export const CardsAppointments = ({ nameProduct, imageProduct, categoryProduct, emailWorker, nameWorker, appointmentId, date, shift, price, emit, client_name, client_email, status, deleted }) => {
 
     const [collapsed, setCollapsed] = useState(true);
 
@@ -13,43 +13,88 @@ export const CardsAppointments = ({ nameProduct, imageProduct, categoryProduct, 
 
     return (
         <div className="card-appointment" key={appointmentId}>
-            <div className="date-category">
-                <div>Date: </div>
-                <div className="date">{date}</div>
-                <div>Shift: </div>
-                <div className="shift">{shift}</div>
-                <div>Category Product : </div>
-                <div className="service"> {categoryProduct }</div>
-                <div className="status"> {status }</div>
-                <div className="client_name">{client_name } </div>
-                <div className="client_email">{client_email } </div>
-            </div>
+
             <button className="button-spoiler" onClick={toggleCollapse}>
                 {collapsed ? "Details" : "Hide"}
             </button>
-            {!collapsed && (
-                <div className="card-appointment-right">
-                    <div className="nameProduct">{nameProduct}</div>
-                    <img className="photo" src={imageProduct} alt={nameProduct} />
-                    <div className="priceProduct">{price} €</div>
-                    <div className="tattoo-artist-card-container">
-                        <div>Worker: </div>
-                        <div className="worker"> {nameWorker}</div>
+            {!collapsed
+
+                ? (
+                    <div className="card-appointment-board">
+
+
+
+
+
+
+                        <div className="left picture">
+                            <div className="nameProduct">{nameProduct}</div>
+                            <img className="photo" src={imageProduct} alt={nameProduct} />
+                        </div>
+
+
+
+                        <div className="center">
+                            <div className="desc-col">Worker: </div>
+                            <div className="nameWorker"> {nameWorker}</div>
+
+                            <div className="desc-col">Price: </div>
+ 
+                            <div className="priceProduct">{price} €</div>
+                            <div className="desc-col">Email: </div>
+
+                            <div className="email"> {emailWorker}</div>
+
+                        </div>
+
+
+                        <div className="right">
+
+                            <div className="desc-col">Date: </div>
+                            <div className="date">{date}</div>
+                            <div className="desc-col">Shift: </div>
+                            <div className="shift">{shift}</div>
+                            <div className="desc-col">Category Product : </div>
+                            <div className="service"> {categoryProduct}</div>
+                            <div className="status"> {status}</div>
+                            <div className="client_name">{client_name} </div>
+                            <div className="client_email">{client_email} </div>
+ 
+                        </div>
+
+
+
+                        <div className="buttons-card-appointments">
+                            <LinkButton
+                                classButton={"button-update-appointment"}
+                                path={"/updateAppointment"}
+                                title={<div className="button-update-appointment" >
+                                    <img src="https://cdn.icon-icons.com/icons2/1558/PNG/512/353430-checkbox-edit-pen-pencil_107516.png" alt="" />
+                                </div>}
+                                emit={() => emit()}
+                            />
+                            <LinkDelete
+                                deleted={() => deleted()}
+                            />
+                        </div>
                     </div>
-                    <div className="email"> {emailWorker}</div>
-                    <LinkButton
-                        classButton={"button-update-appointment"}
-                        path={"/updateAppointment"}
-                        title={<div className="button-update-appointment" >
-                            <img src="https://cdn.icon-icons.com/icons2/1558/PNG/512/353430-checkbox-edit-pen-pencil_107516.png" alt="" />
-                        </div>}
-                        emit={() => emit()}
-                    />
-                    <LinkDelete 
-                    deleted={() => deleted()}
-                    />
-                </div>
-            )}
+                )
+
+                : (
+                    <div className="date-category">
+                        <div>Date: </div>
+                        <div className="date">{date}</div>
+                        <div>Shift: </div>
+                        <div className="shift">{shift}</div>
+                        <div>Category Product : </div>
+                        <div className="service"> {categoryProduct}</div>
+                        <div className="status"> {status}</div>
+                        <div className="client_name">{client_name} </div>
+                        <div className="client_email">{client_email} </div>
+                    </div>
+                )
+
+            }
         </div>
     );
 };
